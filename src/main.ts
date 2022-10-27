d0d1b187db4af2248569b7d3b5fd277fa1545cdf
Alex Sander <57422890+alexsander639@users.noreply.github.com>
2022-10-26 21:47:42 -0400

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('Documentação com Swagger - Loja de Produtos')
+    .setTitle("Documentação com Swagger - Loja de Produtos")
     .setDescription(
-      'Documentação referente ao trabalho da disciplina de Programação Web II, ministrada pelo professor Humberto',
+      "Documentação referente ao trabalho da disciplina de Programação Web II, ministrada pelo professor Humberto"
     )
-    .setVersion('1.0')
-    .addTag('users')
-    .addTag('produtos')
+    .setVersion("1.0")
+    .addTag("users")
+    .addTag("produtos")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
